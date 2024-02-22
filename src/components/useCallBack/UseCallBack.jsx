a8a371cc0015c0476e32de3434372444368f2437
JT Rael <raeljohntimothy@gmail.com>
2024-02-22 16:59:54 +0800

    Shuffle fixed

--- src/components/useCallBack/UseCallBack.jsx
+++ src/components/useCallBack/UseCallBack.jsx
@@ -1,11 +1,14 @@
 import { React, useState, useCallback} from "react";
 import Search from "./Search";
-import shuffle from "./utils";
 
 const UseCallBack = () => {
     const allUsers = ["john", "alex", "shiro", "rin", "yukio"];
 
     const [users, setUsers] = useState(allUsers);
+
+    const handleShuffle = (array) => {
+        setUsers(array.slice().sort(() => Math.random() - 0.5))
+    }
 
     // initial code
     // const handleSearch = (text) => {
@@ -18,17 +21,15 @@ const UseCallBack = () => {
     const handleSearch = useCallback((text) => {
         const filteredUsers = allUsers.filter((user) => user.includes(text));
         setUsers(filteredUsers);
-    }, [users]); // Dependency array, where will the change depent on.
+    }, []); // Dependency array. Which changes will it watch.
 
     return (
         <div className="p-4 grid grid-cols-2 gap-2">
+        <span className="col-span-2 text-white font-bold">UseCallBack</span>
             <div className="grid grid-cols-2 gap-4 col-span-2">
                 <button
                     className="p-4 font-bold bg-sky-400 rounded-lg col-span-1"
-                    onClick={() => {
-                        setUsers(shuffle(allUsers));
-                        console.log("After shuffle: ", users);
-                    }}
+                    onClick={() => handleShuffle(users)}
                 >
                     Shuffle
                 </button>
